5b4c353a0021eee330666f58ef23aeaeedc7c340
Yu seung seon <kouire714@icloud.com>
2024-02-22 08:49:54 +0900

    20240222_study

---
+++ src/j0817_newProject/Hello.java
@@ -0,0 +1,14 @@
+package j0817_newProject;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요!!!!!");
+		System.out.println("Hello, Java");
+		
+		// 한줄 주석입니다.
+		/*
+			여러줄 주석을 쓸수 있습니다.
+			또 설명을 쓸수 있지요....
+		*/
+	}
+}
